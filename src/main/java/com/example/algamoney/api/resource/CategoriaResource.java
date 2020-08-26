7cfe72c5661e303e1cc15f00597c81ddf6f5b512
Gleison Mozart <mozart.gleison@gmail.com>
2020-08-26 17:57:53 -0300

    :construction: CategoriaResouce implementando o metodo de salvar

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -3,8 +3,13 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.model.Categoria;
@@ -14,13 +19,22 @@ import com.example.algamoney.api.repository.CategoriaRepository;
 @RequestMapping("/categorias") //Mapeamento da Requisição
 public class CategoriaResource {
 	
-	@Autowired  //Spring ache e injete a interface categoria aqui
+	@Autowired  //Spring procure  e injete a interface categoria aqui
 	private CategoriaRepository categoriaRepository;  
 	
 	@GetMapping
 	public List<Categoria> listar() {
 		
-		return categoriaRepository.findAll(); // o findAll já foi implementado e está fazendo um select em categoria 
+		return categoriaRepository.findAll(); //Lista todos os registros da tabela categoria
+		
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED) //Resposta informando que o dado foi salvo
+	public void criar(@RequestBody Categoria categoria) {
+		
+    categoriaRepository.save(categoria); //Salva um registr na tabela categoria
+		
 	}
 
 }
