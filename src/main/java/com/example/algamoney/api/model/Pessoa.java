0d87af195ce85dd5260177db1a894743ad55d905
Gleison Mozart <mozart.gleison@gmail.com>
2020-09-09 22:42:26 -0300

    :construct: Model das classes Pessoa e Endereço

---
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -0,0 +1,67 @@
+package com.example.algamoney.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigo;
+	
+	@NotBlank
+	@NotNull
+	@Size(min = 8, max = 50)
+	private String nome;
+	
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (codigo ^ (codigo >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
+	}
+	
+		
+}
