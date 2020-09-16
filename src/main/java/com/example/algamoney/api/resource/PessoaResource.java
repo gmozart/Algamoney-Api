c02fcb63473ea4f30a2200218040b82c29bfbdac
Gleison Mozart <mozart.gleison@gmail.com>
2020-09-16 00:13:00 -0300

    :construct: Pacote Event e Listner para o CRUD da aplicação

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,11 +1,26 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.PostRemove;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoney.api.event.RecursoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 
@@ -15,6 +30,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher; //Publicador de eventos 
 
 	@GetMapping
 	public List<Pessoa> listar() {
@@ -22,4 +40,22 @@ public class PessoaResource {
 		return pessoaRepository.findAll();
 	}
 
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        publisher.publishEvent(new RecursoEvent(this, response, pessoaSalva.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+	}
+
+	@GetMapping("/{codigo}")
+	public Optional<Pessoa> BuscarPessoa(@PathVariable Long codigo) {
+
+		return pessoaRepository.findById(codigo);
+
+	}
+	
+	
 }
