7f2fa499effc2174fd40938b32f8baf92f7722ed
Gleison Mozart <mozart.gleison@gmail.com>
2020-09-17 00:01:40 -0300

    :construct: Categoria e Pessoa Finalizados

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,14 @@
 package com.example.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoney.api.event.RecursoEvent;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 
@@ -24,6 +27,9 @@ public class CategoriaResource {
 
 	@Autowired // Spring procure e injete a interface categoria aqui
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Categoria> listar() {
@@ -34,17 +40,18 @@ public class CategoriaResource {
 
 	@PostMapping
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
+		
 		Categoria categoriaSalva = categoriaRepository.save(categoria); // Salva um registro na tabela categoria
-
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}") // Através da classe Servlet,
+        
+		publisher.publishEvent(new RecursoEvent(this, response, categoriaSalva.getCodigo()));
+		//URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}") // Através da classe Servlet,
 																						// Pegando a requisição ATUAL da
 																						// URI e atribuindo há variavel
 																						// URI
-				.buildAndExpand(categoriaSalva.getCodigo()).toUri(); // adiciona o código na uri
+				//.buildAndExpand(categoriaSalva.getCodigo()).toUri(); // adiciona o código na uri
 		// response.setHeader("Location", uri.toASCIIString()); (Depreciado) // Setar o
 		// header location com a URI que foi pega.
-
-		return ResponseEntity.created(uri).body(categoriaSalva); // Resposta informando que o dado foi salvo e definindo
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva); // Resposta informando que o dado foi salvo e definindo
 																	// o status
 	}
 	
