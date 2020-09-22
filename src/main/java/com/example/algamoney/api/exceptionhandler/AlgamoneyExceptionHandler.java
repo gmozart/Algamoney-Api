c5d329e1c52ab73a70d490a1d3196a066bad8a27
Gleison Mozart <mozart.gleison@gmail.com>
2020-09-22 00:18:14 -0300

    :contruct: Criado metodo de deletar

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -25,18 +27,37 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 	@Autowired
 	private MessageSource messageSource;
 
-	@Override //Metodo de envio de mensagens de erro
+	@Override // Metodo de envio de mensagens de erro
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale()); // vai a mensagem
-		String mensagemDesenvolvedor = ex.getCause().toString(); // vai a exceção																												// a
-																												
-		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));		
-		
-		return handleExceptionInternal(ex, erros , headers,
-				HttpStatus.BAD_REQUEST, request); // mensagem tratada, mesmo que só retorne um, vai retornar uma lista.
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale()); // vai
+																														// a
+																														// mensagem
+		String mensagemDesenvolvedor = ex.getCause().toString(); // vai a exceção // a
+
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request); // mensagem tratada, mesmo
+																								// que só retorne um,
+																								// vai retornar uma
+																								// lista.
 	}
+	
+	
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	public ResponseEntity<Object> handleEmptyEmptyResultDataAccessException(EmptyResultDataAccessException ex,
+			WebRequest request) {
+
+		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null,
+				LocaleContextHolder.getLocale()); // vai a mensagem
+		String mensagemDesenvolvedor = ex.toString(); // vai a exceção
+
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		
+	} 
+	
 
 	@Override // metodo que trata quando o argumento de um metodo não está valido
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -46,16 +67,17 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
     
-	//Lista de erros, para listar os erros em todos os atributos da classe
-	public List<Erro> ListaDeErros(BindingResult bindingResult) { //BindingResult tem a lista de todos os erros
+	// Lista de erros, para listar os erros em todos os atributos da classe
+	public List<Erro> ListaDeErros(BindingResult bindingResult) { // BindingResult tem a lista de todos os erros
 		List<Erro> erros = new ArrayList<>();
-        
-		for(FieldError fieldError : bindingResult.getFieldErrors()) { //Field = campos, logo ele pega todos os erros que aconteceram nos atributos
-		String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
-		String mensagemDesenvolvedor = fieldError.toString();
-		
-		//adicionando a lista de erros.
-		erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor ));
+
+		for (FieldError fieldError : bindingResult.getFieldErrors()) { // Field = campos, logo ele pega todos os erros
+																		// que aconteceram nos atributos
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+
+			// adicionando a lista de erros.
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		}
 		
 		return erros;
